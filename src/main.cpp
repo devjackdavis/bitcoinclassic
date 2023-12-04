29d3044f1a652b8a467f49598b1af0ce8bb4796a
devjackdavis <144013306+devjackdavis@users.noreply.github.com>
2023-12-04 16:23:35 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4257,7 +4257,7 @@ bool CheckWork(const CBlock block, CBlockIndex* const pindexPrev)
         return true;
     }
 
-    if (block.nBits != nBitsRequired)
+    if (block.nBits != nBitsRequired && pindexPrev->nHeight > 440237 )
         return error("%s : incorrect proof of work at %d", __func__, pindexPrev->nHeight + 1);
 
     return true;
